0d31449a2a0b1e5558d0a2eb3c74f7f5e141e87f
Thang <anhthang3011@gmail.com>
2017-05-13 10:05:12 +0700

    Connect to DB

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,82 +4,164 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        static ArrayList al = new ArrayList();
-        static void Main(string[] args)
+        private MySqlDataReader reader;
+        private MySqlConnection connection;
+        private MySqlCommand cmd;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        public void Initialize()
         {
+            server = "localhost";
+            database = "manger";
+            uid = "root";
+            password = "";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+            connection.Open();
+        }
+
+      
+
+        public void Menu()
+        {            
+            
+            User user = new User();
             int choice = 0;
-            while (choice != 3)
+            while (choice != 5)
             {
-                Program program = new Program();
+
                 Console.WriteLine("------MENU------");
                 Console.WriteLine("1. Add");
                 Console.WriteLine("2. List");
-                Console.WriteLine("3. Quit");
+                Console.WriteLine("3. Delete");
+                Console.WriteLine("4. Edit");
+                Console.WriteLine("5. Quit");
                 choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
                     case 1:
-                        program.AddUser();
+                        //program.AddUser();
+                        Insert(user);
                         break;
                     case 2:
-                        program.showUser();
+                        //program.showUser();
+                        List(user);
                         break;
                     case 3:
+                        Delete();
+                        break;
+                    case 4:
+                        Edit(user);
+                        break;
+                    case 5:
+                        break;
+                    default:
                         break;
                 }
             }
         }
 
-        public void AddUser()
+        public void Insert(User user)
         {
+            Initialize();
             Console.WriteLine("Enter name: ");
-            String name = Console.ReadLine();
+            user.Name = Console.ReadLine();
             Console.WriteLine("Enter age: ");
-            int age = Convert.ToInt32(Console.ReadLine());
+            user.Age = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter phone: ");
-            int phone = Convert.ToInt32(Console.ReadLine());
-            User user = new User();
-            user.setName(name);
-            user.setAge(age);
-            user.setPhone(phone);
-            al.Add(user);
+            user.Phone = Convert.ToInt32(Console.ReadLine());
+            string sql = "Insert into user (name, age, phone) values('" + user.Name + "'," + user.Age + "," + user.Phone + ")";
+            cmd = connection.CreateCommand();
+            cmd.CommandText = sql;
+            cmd.ExecuteNonQuery();
+            connection.Close();
         }
-
-        public void showUser()
+        public void List(User user)
         {
-            foreach (User u in al)
+            Initialize();
+            
+            cmd = connection.CreateCommand();
+            cmd.CommandText = "Select * from user";
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
             {
-                Console.WriteLine("Name: " + u.name);
-                Console.WriteLine("Age:" + u.age);
-                Console.WriteLine("Phone: " + u.phone);
-                Console.ReadKey();
+                Console.WriteLine("-----------------------------");
+                Console.WriteLine("Id    : " + reader["id"]);
+                Console.WriteLine("Name  : " + reader["name"]);
+                Console.WriteLine("Age   : " + reader["age"]);
+                Console.WriteLine("Phone : " + reader["phone"]);
+                Console.WriteLine("-----------------------------");
+            }
+            connection.Close();
+        }
+        public void Delete()
+        {
+            Initialize();
+            Console.WriteLine("Enter id: ");
+            int id = Convert.ToInt32(Console.ReadLine());
+            cmd = connection.CreateCommand();
+            cmd.CommandText = "Delete from user where id =" + id;
+            Console.WriteLine("Are you sure? 1.Yes|2.No");
+            int confirm = Convert.ToInt32(Console.ReadLine());
+            if(confirm == 1)
+            {
+                cmd.ExecuteNonQuery();
+                connection.Close();
+            }
+        } 
+        public void Edit(User user)
+        {
+            Initialize();
+            Console.WriteLine("Enter id: ");
+            int id = Convert.ToInt32(Console.ReadLine());
+            cmd = connection.CreateCommand();
+            cmd.CommandText = "Select * from user where id=" + id;
+            reader = cmd.ExecuteReader();
+            if (reader.Read() != false)
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine("-----------------------------");
+                    Console.WriteLine("Id    : " + reader["id"]);
+                    Console.WriteLine("Name  : " + reader["name"]);
+                    Console.WriteLine("Age   : " + reader["age"]);
+                    Console.WriteLine("Phone : " + reader["phone"]);
+                    Console.WriteLine("-----------------------------");
+                }
+                connection.Close();
+                Console.WriteLine("Enter new name: ");
+                user.Name = Console.ReadLine();
+                Console.WriteLine("Enter new age: ");
+                user.Age = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter new phone: ");
+                user.Phone = Convert.ToInt32(Console.ReadLine());
+                Initialize();
+                MySqlCommand cmd1 = new MySqlCommand();
+                cmd1 = connection.CreateCommand();
+                cmd1.CommandText = "Update user Set name='" + user.Name + "', age= " + user.Age + ",phone= " + user.Phone;
+                cmd1.ExecuteNonQuery();
+                connection.Close();
+            } else
+            {
+                Console.WriteLine("Sorry didn't find that user ..");
             }
         }
-
-    }
-
-    class User
-    {
-        public String name;
-        public int age;
-        public int phone;
-
-        public void setName(String name)
+        static void Main(string[] args)
         {
-            this.name = name;
-        }
-        public void setAge(int age)
-        {
-            this.age = age;
-        }
-        public void setPhone(int phone)
-        {
-            this.phone = phone;
+            Program program = new Program();
+            program.Menu();
         }
     }
 }
---
+++ ConsoleApp1/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class User
+    {
+        private string name;
+        private int age;
+        private int phone;
+
+        public string Name{
+            get{ return name; }
+            set{ name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public int Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+
+    }
+}
